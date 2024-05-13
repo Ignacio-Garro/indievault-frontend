62fb34be4b2c61dcfb8f86285ea7320014e3608b
AymenMohamed-Ahmed <115578731+AymenMohamed-Ahmed@users.noreply.github.com>
2024-05-13 16:50:33 +0200

    movie info intial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { MovieInfoComponent } from './movie-info/movie-info.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'movie-info', component: MovieInfoComponent },
+  { path: '', redirectTo: '/movie-info', pathMatch: 'full' },
+];
